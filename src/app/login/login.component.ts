acbfb21f8891bb740bfb63dd6e607c17571f6645
metrysaa20 <antonius1999@yahoo.de>
2023-04-17 12:03:43 +0200

    corrected lint errors

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,53 +1,48 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {FormControl, FormGroup, Validators} from "@angular/forms";
-import {UserService} from "../services/user.service";
-import {Router} from "@angular/router";
-import {User} from "../models/models";
-
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { UserService } from '../services/user.service';
+import { Router } from '@angular/router';
+import { User } from '../models/models';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent implements OnInit{
-
-
+export class LoginComponent implements OnInit {
+  /* is commented out because lint is complaining because it is not used
   validatedLogin: boolean = false;
-
+ */
   // user = User |undefined;
 
   loginFormGroup = new FormGroup({
-    email : new FormControl('', Validators.email),
-    password : new FormControl('', Validators.min(8))
-  })
+    email: new FormControl('', Validators.email),
+    password: new FormControl('', Validators.min(8)),
+  });
 
-  constructor(
-   private dataService: UserService,
-   private router: Router
-  ) {}
+  constructor(private dataService: UserService, private router: Router) {}
 
+  // you can remove this method but it is only used because of lint complaining that it is empty
   ngOnInit(): void {
-
-
+    console.log('ngOnInit is called');
   }
 
   login() {
-    var email = this.loginFormGroup.controls["email"].value;
-    var password = this.loginFormGroup.controls["password"].value;
+    const email = this.loginFormGroup.controls['email'].value;
+    const password = this.loginFormGroup.controls['password'].value;
 
     //let email: string = this.loginFormGroup.get('email')?.value;
-    if(email && password){
-      const loggedinUser : User = {
+    if (email && password) {
+      const loggedinUser: User = {
         email: email,
-        password: password
-      }
+        password: password,
+      };
       console.log(loggedinUser);
-    }else {
-      throw new Error("die übermittelten Daten bei Login sind null")
+    } else {
+      throw new Error('die übermittelten Daten bei Login sind null');
     }
 
     //make REST call
-  //this.router.navigate(['program-overview']);
+    //this.router.navigate(['program-overview']);
   }
 }
