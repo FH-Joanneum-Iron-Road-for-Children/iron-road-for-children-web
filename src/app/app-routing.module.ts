8e1708318be8a33bb2b9892ef7e622d2717c4618
Ihret <theresa.urbanek@edu.fh-joanneum.at>
2023-04-17 16:07:57 +0200

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {Routes} from "@angular/router";
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ProgramComponent } from './program/program.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'program', component: ProgramComponent },
+];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-
-
-
-export class AppRoutingModule {
-
-
-}
+export class AppRoutingModule {}
