6693308141e7bbc8e8fd63bd6768b42e496f15d4
Elena Schnedlitz <elena.schnedlitz@edu.fh-joanneum.at>
2023-04-17 16:25:04 +0200

    implement dummy-dialog, add angular material, add default for routing example,

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {Routes} from "@angular/router";
+import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+//insert here more routes when needed
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule],
 })
-
-
-
-export class AppRoutingModule {
-
-
-}
+export class AppRoutingModule {}
