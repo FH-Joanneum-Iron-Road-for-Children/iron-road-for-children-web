95a1c3a6656aeabe334f311d468a0bc0aeeb9753
Islam Hemida <isi@Islams-MacBook-Pro.local>
2023-04-24 17:38:01 +0200

    running dummy test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,48 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { ReactiveFormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
-import { MatInputModule } from '@angular/material/input';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
-import { MatDialogModule } from '@angular/material/dialog';
-import { HttpClientModule } from '@angular/common/http';
-
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { ProgramComponent } from './program/program.component';
-import { DummyDialogComponent } from './dummy-dialog/dummy-dialog.component';
-import { RouterLink } from '@angular/router';
-import { ProgramFiltersComponent } from './program/program-filters/program-filters.component';
-import { MatLegacyChipsModule } from '@angular/material/legacy-chips';
-import { MatChipsModule } from '@angular/material/chips';
-import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    ProgramComponent,
-    DummyDialogComponent,
-    ProgramFiltersComponent,
+    AppComponent
   ],
-
   imports: [
-    HttpClientModule,
-    BrowserModule,
-    ReactiveFormsModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatInputModule,
-    MatButtonModule,
-    MatDialogModule,
-    RouterLink,
-    MatLegacyChipsModule,
-    MatChipsModule,
-    MatIconModule,
+    BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { UserService } from '../services/user.service';
 import { Router } from '@angular/router';
 import { User } from '../models/models';
-import { MatDialog } from '@angular/material/dialog';
-import { DummyDialogComponent } from '../dummy-dialog/dummy-dialog.component';
-
 
 @Component({
   selector: 'app-login',
@@ -23,12 +20,7 @@ export class LoginComponent implements OnInit {
     password: new FormControl('', Validators.min(8)),
   });
 
-
-   constructor(
-    private userService: UserService,
-    private router: Router,
-    public dialog: MatDialog
-  ) {}
+  constructor(private dataService: UserService, private router: Router) {}
 
   // you can remove this method but it is only used because of lint complaining that it is empty
   ngOnInit(): void {
@@ -51,22 +43,7 @@ export class LoginComponent implements OnInit {
     }
 
     //make REST call
-    //this is how to route to other components
-    this.router.navigate(['program']);
-  }
-
-  openDummyDialog() {
-    //opens the DialogComponent
-    //you can further edit the look with f.e. saying the width is max 250px and so on
-    //const refDialog =
-    this.dialog.open(DummyDialogComponent, {
-      disableClose: true,
-    });
-
-    //needed if you want to do something with the data after closing the dialog
-    //normally the rest call happens in the Dialog but it depends on the data
-    // refDialog.afterClosed().subscribe(result => {
-    //   console.log(`Dialog result: ${result}`);
-    // });
+    //this.router.navigate(['program-overview']);
+    
   }
 }
