66a7abed4480f229aa5c55a437025d4cd72dab1c
Islam-Hemida <islam.hemida@edu.fh-joanneum.at>
2023-04-25 10:31:21 +0200

    try to fix MatDialog Test issue.

--- src/app/program/delete-dialog/delete-dialog.component.spec.ts
+++ src/app/program/delete-dialog/delete-dialog.component.spec.ts
@@ -1,6 +1,8 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { DeleteDialogComponent } from './delete-dialog.component';
+import {DeleteDialogComponent} from './delete-dialog.component';
+import {NO_ERRORS_SCHEMA} from "@angular/core";
+import {MAT_DIALOG_DATA, MAT_DIALOG_DEFAULT_OPTIONS, MatDialogModule, MatDialogRef} from "@angular/material/dialog";
 
 describe('DeleteDialogComponent', () => {
   let component: DeleteDialogComponent;
@@ -8,8 +10,16 @@ describe('DeleteDialogComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [MatDialogModule],
       declarations: [DeleteDialogComponent],
+      schemas: [NO_ERRORS_SCHEMA],
+      providers: [
+        {provide: MatDialogRef, useValue: {}},
+        {provide: MatDialogModule, useValue: {}},
+        {provide: MAT_DIALOG_DATA, useValue: {}},
+        {provide: MAT_DIALOG_DEFAULT_OPTIONS, useValue: {hasBackdrop: false}}],
     }).compileComponents();
+
 
     fixture = TestBed.createComponent(DeleteDialogComponent);
     component = fixture.componentInstance;
