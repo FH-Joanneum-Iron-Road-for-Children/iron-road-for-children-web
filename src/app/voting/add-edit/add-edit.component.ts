2e37a44b62b983802aeb684270f3a0bd5ea28e96
Elena Schnedlitz <elena.schnedlitz@edu.fh-joanneum.at>
2023-04-25 10:47:40 +0200

    align items next to each other, delete unused css code, add example for *ngFor

--- src/app/voting/add-edit/add-edit.component.ts
+++ src/app/voting/add-edit/add-edit.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-add-edit',
   templateUrl: './add-edit.component.html',
-  styleUrls: ['./add-edit.component.css']
+  styleUrls: ['./add-edit.component.css'],
 })
 export class AddEditComponent {
+  addEditFormGroup = new FormGroup({
+    votingName: new FormControl(''),
+    participant: new FormControl(''),
+  });
 
-  sendDataToRest() {
+  listOfStrings: string[] = ['nfweii', 'fiwejfoe'];
 
-  }
+  constructor() {}
+
+  //add method to add participant
+  //addToList(participant:string){
+  //list.push(value);}
+
+  sendDataToRest() {}
 }
