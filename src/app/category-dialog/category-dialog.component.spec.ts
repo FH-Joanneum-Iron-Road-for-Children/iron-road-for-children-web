4c9b14d4f5e8d468aa90ca630eb79720f1ce6ecb
bognern <127208234+bognern@users.noreply.github.com>
2023-05-01 17:58:43 +0200

    #21 Fix failing tests

--- src/app/category-dialog/category-dialog.component.spec.ts
+++ src/app/category-dialog/category-dialog.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CategoryDialogComponent } from './category-dialog.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('CategoryDialogComponent', () => {
   let component: CategoryDialogComponent;
@@ -9,6 +10,7 @@ describe('CategoryDialogComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [CategoryDialogComponent],
+      schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
 
     fixture = TestBed.createComponent(CategoryDialogComponent);
