217bc553f4a041588d0bf14c843a10fee811637d
bognern <127208234+bognern@users.noreply.github.com>
2023-05-02 16:28:37 +0200

    #22 Fill Program View with test data

--- src/app/program/program-view/program-view.component.ts
+++ src/app/program/program-view/program-view.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { EventDto } from '../../models/models';
+import { EVENT_DATA } from '../../services/test-data';
 
 @Component({
   selector: 'app-program-view',
   templateUrl: './program-view.component.html',
   styleUrls: ['./program-view.component.css'],
 })
-export class ProgramViewComponent {}
+export class ProgramViewComponent {
+  public events: EventDto[] = EVENT_DATA;
+}
