beeb7d24bedc72dc5dbcfd9a8894d34bacfc44ad
Islam-Hemida <islam.hemida@edu.fh-joanneum.at>
2023-05-02 12:37:27 +0200

    small merge conflicts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-import { MatInputModule } from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDialogModule } from '@angular/material/dialog';
 import { HttpClientModule } from '@angular/common/http';
-
-
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import {MatInputModule} from "@angular/material/input";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {MatButtonModule} from "@angular/material/button";
-import {HttpClientModule} from "@angular/common/http";
 import { AddEditComponent } from './voting/add-edit/add-edit.component';
 import { ProgramComponent } from './program/program.component';
 import { DummyDialogComponent } from './dummy-dialog/dummy-dialog.component';
@@ -36,7 +28,7 @@ import {MatDialogModule} from "@angular/material/dialog";
     ProgramComponent,
     DummyDialogComponent,
     ProgramFiltersComponent,
-    AddEditComponent
+    AddEditComponent,
     DeleteDialogComponent,
     NavigationComponent,
   ],
