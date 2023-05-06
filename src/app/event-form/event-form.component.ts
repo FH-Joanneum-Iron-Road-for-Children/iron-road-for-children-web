77bab100741e86f73bf7c6897508c07222bd0a13
bognern <127208234+bognern@users.noreply.github.com>
2023-05-06 22:07:30 +0200

    #22 Add DateTime picker to Event forms

--- src/app/event-form/event-form.component.ts
+++ src/app/event-form/event-form.component.ts
@@ -3,11 +3,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 import { EventCategoryDto, EventDto, EventLocationDto } from '../models/models';
-import {
-  CATEGORY_DATA,
-  EVENT_DATA,
-  LOCATION_DATA,
-} from '../test-data/test-data';
+import { CATEGORY_DATA, LOCATION_DATA } from '../test-data/test-data';
 
 @Component({
   selector: 'app-event-form',
@@ -21,22 +17,23 @@ export class EventFormComponent implements OnInit {
     title: new FormControl('', Validators.min(1)),
     description: new FormControl('', Validators.min(1)),
     location: new FormControl('', Validators.min(1)),
-    date: new FormControl('', Validators.min(1)),
-    startTime: new FormControl('', Validators.min(1)),
-    endTime: new FormControl('', Validators.min(1)),
+    startDateTime: new FormControl('', Validators.min(1)),
+    endDateTime: new FormControl('', Validators.min(1)),
   });
 
   title: string | undefined;
-  date: string | undefined;
-  startTime: string | undefined;
-  endTime: string | undefined;
+  date: any;
   category: number | undefined;
   location: number | undefined;
 
   public categories: EventCategoryDto[] = CATEGORY_DATA;
   public locations: EventLocationDto[] = LOCATION_DATA;
+  minDate: Date;
 
-  constructor(private router: Router, public dialog: MatDialog) {}
+  constructor(private router: Router, public dialog: MatDialog) {
+    const currentYear = new Date().getFullYear();
+    this.minDate = new Date(currentYear, 0, 1);
+  }
 
   ngOnInit(): void {
     console.log(this.event);
