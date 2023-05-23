d1777ff94b56ac438d0702c1550b6d738eae5eb5
Gerwald Gindra-Vady <gerwald.gindra-vady@edu.fh-joanneum.at>
2023-05-23 11:48:53 +0200

    #15 view voting - image can be selected and will be shown

--- src/app/voting/voting-element/voting-card/voting-card.component.ts
+++ src/app/voting/voting-element/voting-card/voting-card.component.ts
@@ -7,23 +7,45 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class VotingCardComponent {
   @Output() deleteCardEvent = new EventEmitter<string>();
+
   deleteCard() {
     this.deleteCardEvent.emit();
   }
+
   @Input()
   get name(): string {
     return this._name;
   }
+
   set name(name: string) {
     this._name = (name && name.trim()) || '<no name set>';
   }
+
   private _name = '';
+
   @Input()
   get text(): string {
     return this._text;
   }
+
   set text(name: string) {
-    this._text = (name && name.trim()) || '<no name set>';
+    this._text = (name && name.trim()) || '<Bitte Text hinzufügen>';
   }
+
   private _text = '';
+
+  selectedImage!: File;
+  imageUrl!: string;
+
+  fileName = '';
+
+  onFileSelected(event: any) {
+    this.selectedImage = event.target.files[0];
+
+    const reader = new FileReader();
+    reader.onload = (e: any) => {
+      this.imageUrl = e.target.result;
+    };
+    reader.readAsDataURL(this.selectedImage);
+  }
 }
