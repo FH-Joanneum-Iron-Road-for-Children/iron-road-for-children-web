8e720cb7f16a9f97c16b4cafadbdf3103008a7eb
bognern <127208234+bognern@users.noreply.github.com>
2023-05-28 08:36:03 +0200

    #90 Get data from event service

--- src/app/program/shared/event-dialog/location-dialog/location-dialog.component.ts
+++ src/app/program/shared/event-dialog/location-dialog/location-dialog.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { EventLocationDto } from '../../../../models/models';
-import { LOCATION_DATA } from '../../../../test-data/test-data';
+import { EventService } from '../../../../services/event.service';
 
 @Component({
   selector: 'app-location-dialog',
   templateUrl: './location-dialog.component.html',
   styleUrls: ['./location-dialog.component.css'],
 })
-export class LocationDialogComponent {
-  locationList: EventLocationDto[] = LOCATION_DATA;
+export class LocationDialogComponent implements OnInit {
+  locationList: EventLocationDto[] = [];
 
-  saveLocations() {}
+  constructor(private eventService: EventService) {}
+
+  ngOnInit(): void {
+    this.locationList = this.eventService.getLocations();
+  }
+
+  saveLocations() {
+    // TODO
+  }
 }
--- src/app/program/view/program.component.ts
+++ src/app/program/view/program.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EventDto } from '../../models/models';
 
 @Component({
   selector: 'app-footer',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./program.component.css'],
 })
 export class ProgramComponent implements OnInit {
+  public events: EventDto[] = [];
+
   ngOnInit(): void {
     console.log('ngOnInit is called');
   }
