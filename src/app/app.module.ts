cebedd32bfb9c928be07f10aa4d1d90cfe53442c
bognern <127208234+bognern@users.noreply.github.com>
2023-05-29 19:17:21 +0200

    #90 Add date conversion service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -44,7 +44,10 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { ProgramViewComponent } from './program/view/program-view/program-view.component';
 import { EditEventComponent } from './program/edit/edit-event/edit-event.component';
 import { EventDialogComponent } from './program/shared/event-dialog/event-dialog.component';
+import { registerLocaleData } from '@angular/common';
+import localeAt from '@angular/common/locales/de-AT';
 
+registerLocaleData(localeAt, 'de-AT');
 
 @NgModule({
   declarations: [
