43663c09ac33af833931f9fbcf8200a883d33e48
Elena Schnedlitz <elena.schnedlitz@edu.fh-joanneum.at>
2023-05-30 16:47:10 +0200

    export url to environment.ts for better and quicker accessing the url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,5 @@
-export const environment = {};
+export const environment = {
+  baseUrl: 'https://backend.irfc-test.st-ki.at/api/',
+};
+
+// export const baseUrl = 'https://backend.irfc-test.st-ki.at/api/';
