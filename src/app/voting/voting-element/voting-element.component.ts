9b195aeb5f6703824b767baefabbc8eb2d5a4d26
bognern <127208234+bognern@users.noreply.github.com>
2023-05-31 22:35:14 +0200

    #86 Open edit voting dialog

--- src/app/voting/voting-element/voting-element.component.ts
+++ src/app/voting/voting-element/voting-element.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { ConfirmDialogService } from '../../services/confirm-dialog.service';
+import { VotingAddEditComponent } from '../add-edit/voting-add-edit.component';
 
 @Component({
   selector: 'app-voting-element',
@@ -8,10 +9,20 @@ import { ConfirmDialogService } from '../../services/confirm-dialog.service';
   styleUrls: ['./voting-element.component.css'],
 })
 export class VotingElementComponent {
+  isActive: boolean | undefined;
+
   constructor(
     private dialog: MatDialog,
     private confirmDialogService: ConfirmDialogService
   ) {}
+
+  openEditVotingDialog() {
+    this.dialog.open(VotingAddEditComponent, {
+      disableClose: true,
+      width: '45rem',
+      height: '30rem',
+    });
+  }
 
   openDeleteVotingDialog() {
     const msg = 'Voting endgültig löschen?'; // TODO: Show voting title
