c8677d5694ae00c3e1fe276e26a3fc4da3c030f3
Daniel_Zarnhofer <daniel.zarnhofer@edu.fh-joanneum.at>
2025-04-28 23:51:59 +0200

    fix tests for news page branch

--- src/app/highlight-upload/highlight-upload.component.spec.ts
+++ src/app/highlight-upload/highlight-upload.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HighlightUploadComponent } from './highlight-upload.component';
+import { TestModule } from '../testing/test.module';
 
 describe('HighlightUploadComponent', () => {
   let component: HighlightUploadComponent;
@@ -9,6 +10,7 @@ describe('HighlightUploadComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [HighlightUploadComponent],
+      imports: [TestModule], // Add any necessary imports here
     }).compileComponents();
 
     fixture = TestBed.createComponent(HighlightUploadComponent);
