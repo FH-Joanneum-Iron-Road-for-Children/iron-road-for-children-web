018e736597c707bcc04389eeafdfde057fcbd897
Noodle-prog <valentina.breithuber@edu.fh-joanneum.at>
2025-05-13 12:49:46 +0200

    add import custom elements schema

--- src/app/news-page/view/video/video/video.component.spec.ts
+++ src/app/news-page/view/video/video/video.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { VideoComponent } from './video.component';
 import { TestModule } from 'src/app/testing/test.module';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('ComponentNameComponent', () => {
   let component: VideoComponent;
@@ -11,6 +12,7 @@ describe('ComponentNameComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [VideoComponent],
       imports: [TestModule], // Add any necessary imports here
+      schemas: [CUSTOM_ELEMENTS_SCHEMA], // Add any necessary schemas here
     }).compileComponents();
 
     fixture = TestBed.createComponent(VideoComponent);
