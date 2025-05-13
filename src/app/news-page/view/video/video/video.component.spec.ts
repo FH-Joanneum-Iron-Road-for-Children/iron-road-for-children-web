2fd0900dcd14bafe86329f35033c347c8ed530e2
Daniel Zarnhofer <daniel.zarnhofer@edu.fh-joanneum.at>
2025-05-13 13:05:13 +0200

    Fixes to test setup

--- src/app/news-page/view/video/video/video.component.spec.ts
+++ src/app/news-page/view/video/video/video.component.spec.ts
@@ -3,16 +3,30 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { VideoComponent } from './video.component';
 import { TestModule } from 'src/app/testing/test.module';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { NewsService } from 'src/app/news-page/news-page.service';
+import { of } from 'rxjs';
 
 describe('ComponentNameComponent', () => {
   let component: VideoComponent;
   let fixture: ComponentFixture<VideoComponent>;
+  let newsServiceMock: Partial<NewsService>;
 
   beforeEach(async () => {
+    newsServiceMock = {
+      getVideo: jasmine
+        .createSpy('getVideo')
+        .and.returnValue(of({ videoId: 1, altText: 'Test', path: 'test.mp4' })),
+    };
+
     await TestBed.configureTestingModule({
       declarations: [VideoComponent],
       imports: [TestModule], // Add any necessary imports here
-      schemas: [CUSTOM_ELEMENTS_SCHEMA], // Add any necessary schemas here
+      providers: [
+        { provide: MatDialogRef, useValue: {} },
+        { provide: NewsService, useValue: newsServiceMock },
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(VideoComponent);
